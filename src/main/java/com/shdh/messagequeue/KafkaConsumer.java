aeb2c7757d785cc8b4c3dd0e6b1b7551d1c99a95
kdh <troth1532@naver.com>
2021-08-14 20:16:24 +0900

    add kafka consumer & consumerConfig

---
+++ src/main/java/com/shdh/messagequeue/KafkaConsumer.java
@@ -0,0 +1,51 @@
+package com.shdh.messagequeue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shdh.jpa.CatalogEntity;
+import com.shdh.jpa.CatalogRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+	CatalogRepository repository;
+	
+	@Autowired
+	public KafkaConsumer(CatalogRepository repository) {
+		this.repository = repository;
+	}
+	
+	
+	@KafkaListener(topics = "example-catalog-topic")
+	public void updateQty(String kafkaMessage) {
+		log.info("Kafka Message: -> ;"+kafkaMessage);
+		
+		Map<Object, Object> map = new HashMap<>();
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+		}catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		CatalogEntity entity = repository.findByProductId((String)map.get("productId"));
+		if(entity != null) {
+			entity.setStock(entity.getStock() - (Integer)map.get("qty"));
+			repository.save(entity);
+		}
+		
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/shdh/messagequeue/KafkaConsumerConfig.java
@@ -0,0 +1,42 @@
+package com.shdh.messagequeue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+	// 접속하고자 하는 카프카 서버 설정
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		Map<String, Object> properties = new HashMap<>();
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerFroupId");
+		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+		return new DefaultKafkaConsumerFactory<>(properties);
+	}
+
+	// 카프카 컨슈머 리스너 빈 등록
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory 
+			= new ConcurrentKafkaListenerContainerFactory<String, String>();
+		
+		kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+		
+		
+		return kafkaListenerContainerFactory;
+	}
+}
